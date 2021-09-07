71aa84e4ead3668d006c2dbf7ba218844359993a
Chris McCutcheon <cmccutcheon2015@gmail.com>
2021-09-07 13:57:10 -0600

    make the title look ok

--- src/components/Title/styles.js
+++ src/components/Title/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledTitle = styled.h2`
-  color: blue;
+  color: #46d6fa;
+  font-family: (sans-serif);
+
+  text-shadow: 0.05em 0.05em 0 yellow,
+  0.1em 0.1em 0 red;
 `;
